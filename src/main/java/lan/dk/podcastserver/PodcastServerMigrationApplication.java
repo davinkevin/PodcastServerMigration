fd64856181375f67f9c23f4558b60f2aa11a9043
Davin Kevin <davin.kevin@gmail.com>
2016-03-30 06:27:54 +0200

    Initial commit

---
+++ src/main/java/lan/dk/podcastserver/PodcastServerMigrationApplication.java
@@ -0,0 +1,12 @@
+package lan.dk.podcastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastServerMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastServerMigrationApplication.class, args);
+	}
+}
---
+++ src/test/java/lan/dk/podcastserver/PodcastServerMigrationApplicationTests.java
@@ -0,0 +1,16 @@
+package lan.dk.podcastserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PodcastServerMigrationApplication.class)
+public class PodcastServerMigrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
